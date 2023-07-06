4e78a112955ff8428a993c7411b4a58e21b816ce
Laith Qasem <67019009+Laithqasem@users.noreply.github.com>
2023-07-07 02:41:52 +0300

    left: schedule and students

--- app/src/main/java/com/example/finalproject/Instructor_Activity.java
+++ app/src/main/java/com/example/finalproject/Instructor_Activity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -21,6 +23,8 @@ public class Instructor_Activity extends AppCompatActivity {
     String user_email="";
     DataBaseHelper dataBaseHelper = new DataBaseHelper(
             Instructor_Activity.this,"TRAINING_CENTER",null,1);
+
+    TextView nameTextView,emailTextView,addressTextView,degreeTextView,mobileTextView,specializationTextView,canteachTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,11 +133,46 @@ public class Instructor_Activity extends AppCompatActivity {
         }
 
 
+
+
+
         ImageView profilePhoto = findViewById(R.id.instructor_profile_image);
+        emailTextView  = findViewById(R.id.emailTextView);
+         addressTextView = findViewById(R.id.addressTextView);
+         specializationTextView = findViewById(R.id.specializationTextView);
+         mobileTextView = findViewById(R.id.mobileTextView);
+         degreeTextView = findViewById(R.id.degreeTextView);
+         nameTextView = findViewById(R.id.nameTextView);
+         canteachTextView = findViewById(R.id.canteachTextView);
+         profilePhoto = findViewById(R.id.instructor_profile_image);
+
         Bitmap bitmap = BitmapFactory.decodeByteArray(user_personal_photo, 0, user_personal_photo.length);
         profilePhoto.setImageBitmap(bitmap);
 
+        emailTextView.setText(user_email);
+        emailTextView.setPaintFlags(emailTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
 
+        addressTextView.setText(user_address);
+        addressTextView.setPaintFlags(addressTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+        specializationTextView.setText(user_specialization);
+        specializationTextView.setPaintFlags(specializationTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+        mobileTextView.setText(user_mobile_no);
+        mobileTextView.setPaintFlags(mobileTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+        degreeTextView.setText(user_degree);
+        degreeTextView.setPaintFlags(degreeTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+        String name = user_first_name + " " + user_last_name;
+        nameTextView.setText(name);
+        nameTextView.setPaintFlags(nameTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+
+
+        String canTeachString = user_canTeach.substring(2, user_canTeach.length());
+        canteachTextView.setText(canTeachString);
+        canteachTextView.setPaintFlags(canteachTextView.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
 
 
 
