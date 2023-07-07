36dcb80b55aeade6c902aea7fc58202109c2b997
Laith Qasem <67019009+Laithqasem@users.noreply.github.com>
2023-07-07 03:51:00 +0300

    left: schedule and students

--- app/src/main/java/com/example/finalproject/InstructorScheduleActivity.java
+++ app/src/main/java/com/example/finalproject/InstructorScheduleActivity.java
@@ -1,14 +1,28 @@
 package com.example.finalproject;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Calendar;
+
 public class InstructorScheduleActivity extends AppCompatActivity {
     String user_email="";
+    Button DatePicker;
+    DatePickerDialog datePickerDialog1;
+    TextView DateTextView;
+    String pickedDay="", pickedMonth="", pickedYear="",fullPickedDate="";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +62,120 @@ public class InstructorScheduleActivity extends AppCompatActivity {
             }
             return false;
         });
+
+        DatePicker = findViewById(R.id.DatePicker);
+        DateTextView = findViewById(R.id.textView8);
+
+        initDatePicker();
+        DatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog1.show();
+            }
+        });
+        DatePicker.setText(getTodaysDate());
+        String PickedDate = DatePicker.getText().toString();
+        if(PickedDate.isEmpty()){
+            DateTextView.setTextColor(Color.RED);
+        }
+        else{
+            DateTextView.setTextColor(Color.WHITE);
+        }
+
+
+
+
+
     }
+
+
+    private String getTodaysDate() {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    public void initDatePicker() {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(android.widget.DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                DateTextView.setText(date);
+                fullPickedDate = DateTextView.getText().toString();
+                String[] dateParts = fullPickedDate.split(" ");
+                pickedMonth  = dateParts[0];
+                pickedDay  = dateParts[1];
+                pickedYear  = dateParts[2];
+
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog1 = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+     }
+
+    public String makeDateString(int day, int month, int year) {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    public int getInvMonthFormat(String month){
+        if(month.equals("JAN")) return 1;
+        if(month.equals("FEB")) return 2;
+        if(month.equals("MAR")) return 3;
+        if(month.equals("APR")) return 4;
+        if(month.equals("MAY")) return 5;
+        if(month.equals("JUN")) return 6;
+        if(month.equals("JUL")) return 7;
+        if(month.equals("AUG")) return 8;
+        if(month.equals("SEP")) return 9;
+        if(month.equals("OCT")) return 10;
+        if(month.equals("NOV")) return 11;
+        if(month.equals("DEC")) return 12;
+        return -1;
+    }
+
+    private String getMonthFormat(int month) {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+
+
 }
